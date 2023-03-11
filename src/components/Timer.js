d8c2454c6385f077022e1204c7a8bb0767dab8a4
Andriy Lytvyn <andriy@devsalsa.com>
2023-03-11 10:22:39 +0200

    fix issues  HW-4

---
+++ src/components/Timer.js
@@ -0,0 +1,56 @@
+import { Component } from "react";
+
+class Timer extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {seconds: 0, time: {}};
+    }
+
+    secondsToTime(secs){
+      let hours = Math.floor(secs / (60 * 60));
+  
+      let divisor_for_minutes = secs % (60 * 60);
+      let minutes = Math.floor(divisor_for_minutes / 60);
+  
+      let divisor_for_seconds = divisor_for_minutes % 60;
+      let seconds = Math.ceil(divisor_for_seconds);
+  
+      let obj = {
+        "h": hours,
+        "m": minutes,
+        "s": seconds
+      };
+      return obj;
+    }
+  
+    componentDidMount() {
+      this.timerID = setInterval(
+        () => this.tickSeconds(),
+        1000
+      );
+    }
+  
+    componentWillUnmount() {
+      clearInterval(this.timerID);
+    }
+  
+    tickSeconds() {
+      let {seconds} = this.state;
+      seconds++;
+
+      this.setState({
+        seconds: seconds,
+        time: this.secondsToTime(seconds),
+      });
+    }
+  
+    render() {
+      return (
+        <div>
+          <h2>Ви знаходитесь на сайті: {this.state.time.h} : {this.state.time.m} : {this.state.time.s}.</h2>
+        </div>
+      );
+    }
+  }
+
+  export default Timer;
